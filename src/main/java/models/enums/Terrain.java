1c7d565c9e537d5eae99a66319c49f5e6cc8e002
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-04-20 18:14:58 +0430

    Add Terrain enum

---
+++ src/main/java/models/enums/Terrain.java
@@ -0,0 +1,29 @@
+package models.enums;
+
+public enum Terrain {
+    DESERT("Desert", 0, 0, 0, 1, -1/3, true),
+    GRASSLAND("Grassland", 2, 0, 0, 1, -1/3, true),
+    HILL("Hill", 0, 2, 0, 2, 1/4, true),
+    MOUNTAIN("Mountain", 0, 0, 0, 0, 0, false),
+    OCEAN("Ocean", 0, 0, 0, 0, 0, false),
+    PLAIN("Plain", 1, 1, 0, 1, -1/3, true),
+    SNOW("Snow", 0, 0, 0, 1, -1/3, true),
+    TUNDRA("Tundra", 1, 0, 0, 1, -1/3, true);
+    private String name;
+    private int food;
+    private int production;
+    private int gold;
+    private int movementCost;
+    private float combatModifiers;
+    private boolean accessible;
+
+    Terrain(String name, int food, int production, int gold, int movementCost, float combatModifiers, boolean accessible) {
+        this.name = name;
+        this.food = food;
+        this.production = production;
+        this.gold = gold;
+        this.movementCost = movementCost;
+        this.combatModifiers = combatModifiers;
+        this.accessible = accessible;
+    }
+}
