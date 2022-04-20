91b2ab06c144c7786cbc707a0119b67e4604695d
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-04-20 18:26:42 +0430

    Add TerrainFeature enum

---
+++ src/main/java/models/enums/TerrainFeature.java
@@ -0,0 +1,29 @@
+package models.enums;
+
+public enum TerrainFeature {
+    FOOD_PLAIN("Food plain", 2, 0, 0, 1, -1/3, true),
+    FOREST("Forest", 1, 1, 0, 2, 1/4, true),
+    ICE("Ice", 0, 0, 0, 0, 0, false),
+    JUNGLE("Jungle", 1, -1, 0, 2, 1/4, true),
+    MARSH("Marsh", -1, 0, 0, 2, -1/3, true),
+    OASIS("Oasis", 3, 0, 1, 1, -1/3, true);
+
+    private String name;
+    private int food;
+    private int production;
+    private int gold;
+    private int movementCost;
+    private float combatModifier;
+    private boolean accessible;
+
+    TerrainFeature(String name, int food, int production, int gold, int movementCost, float combatModifier, boolean accessible) {
+        this.name = name;
+        this.food = food;
+        this.production = production;
+        this.gold = gold;
+        this.movementCost = movementCost;
+        this.combatModifier = combatModifier;
+        this.accessible = accessible;
+
+    }
+}
