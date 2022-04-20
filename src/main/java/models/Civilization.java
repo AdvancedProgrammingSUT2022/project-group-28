f44703b26cee284d98df5b8272f11a68beb37e21
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-04-20 19:19:15 +0430

    Add Civilization model

---
+++ src/main/java/models/Civilization.java
@@ -0,0 +1,13 @@
+package models;
+
+import models.units.Unit;
+
+import java.util.ArrayList;
+
+public class Civilization {
+    // TODO: Add User field
+    private String NAME;
+    private ArrayList<Unit> units;
+    // TODO: Complete fields
+    // TODO: Add constructor
+}
