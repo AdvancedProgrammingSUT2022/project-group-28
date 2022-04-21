f559efbe7259f74a7d969bb8d297d6bf3fe53866
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-04-21 18:40:06 +0430

    Change tile constructor

--- src/main/java/models/Tile.java
+++ src/main/java/models/Tile.java
@@ -12,11 +12,11 @@ public class Tile {
     ImprovementTemplate improvement;
     Project project;
     // TODO: add Ruin, City
-    public Tile(Terrain terrain, TerrainFeature terrainFeature, Resource resource) {
+    public Tile(Terrain terrain, TerrainFeature terrainFeature, Resource resource, Military military, Civilian civilian) {
         this.terrain = terrain;
         this.terrainFeature = terrainFeature;
-        this.military = null;
-        this.civilian = null;
+        this.military = military;
+        this.civilian = civilian;
         this.resource = resource;
         this.improvement = null;
         this.project = null;
