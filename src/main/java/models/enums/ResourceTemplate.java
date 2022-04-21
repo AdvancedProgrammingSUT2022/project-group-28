601c76264cc9ca31c203872d08c4e7e12359c818
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-04-21 18:04:53 +0430

    Add improvement field

--- src/main/java/models/enums/ResourceTemplate.java
+++ src/main/java/models/enums/ResourceTemplate.java
@@ -1,25 +1,25 @@
 package models.enums;
 
 public enum ResourceTemplate {
-    BANANA("Banana", "BONUS", 1, 0, 0),
-    COW("Cow", "BONUS", 1, 0, 0),
-    GAZELLE("Gazelle", "BONUS", 1, 0, 0),
-    SHEEP("Sheep", "BONUS", 2, 0, 0),
-    WHEAT("Wheat", "BONUS", 1, 0, 0),
-    COAL("Coal", "STRATEGIC", 0, 1, 0),
-    HORSE("Horse", "STRATEGIC", 0, 1, 0),
-    IRON("Iron", "STRATEGIC", 0, 1, 0),
-    COTTON("Cotton", "LUXURY", 0, 0, 2),
-    DYE("Dye", "LUXURY", 0, 0, 2),
-    FUR("Fur", "LUXURY", 0, 0, 2),
-    GEM("Gem", "LUXURY", 0, 0, 3),
-    GOLD("Gold", "LUXURY", 0, 0, 2),
-    INCENSE("Incense", "LUXURY", 0, 0, 2),
-    IVORY("Ivory", "LUXURY", 0, 0, 2),
-    MARBLE("Marble", "LUXURY", 0, 0, 2),
-    SILK("Silk", "LUXURY", 0, 0, 2),
-    SILVER("Silver", "LUXURY", 0, 0, 2),
-    SUGAR("Sugar", "LUXURY", 0, 0, 2);
+    BANANA("Banana", "BONUS", 1, 0, 0, ImprovementTemplate.PLANTATION),
+    COW("Cow", "BONUS", 1, 0, 0, ImprovementTemplate.PASTURE),
+    GAZELLE("Gazelle", "BONUS", 1, 0, 0, ImprovementTemplate.CAMP),
+    SHEEP("Sheep", "BONUS", 2, 0, 0, ImprovementTemplate.PASTURE),
+    WHEAT("Wheat", "BONUS", 1, 0, 0, ImprovementTemplate.FARM),
+    COAL("Coal", "STRATEGIC", 0, 1, 0, ImprovementTemplate.MINE),
+    HORSE("Horse", "STRATEGIC", 0, 1, 0, ImprovementTemplate.PASTURE),
+    IRON("Iron", "STRATEGIC", 0, 1, 0, ImprovementTemplate.MINE),
+    COTTON("Cotton", "LUXURY", 0, 0, 2, ImprovementTemplate.PLANTATION),
+    DYE("Dye", "LUXURY", 0, 0, 2, ImprovementTemplate.PLANTATION),
+    FUR("Fur", "LUXURY", 0, 0, 2, ImprovementTemplate.CAMP),
+    GEM("Gem", "LUXURY", 0, 0, 3, ImprovementTemplate.MINE),
+    GOLD("Gold", "LUXURY", 0, 0, 2, ImprovementTemplate.MINE),
+    INCENSE("Incense", "LUXURY", 0, 0, 2, ImprovementTemplate.PLANTATION),
+    IVORY("Ivory", "LUXURY", 0, 0, 2, ImprovementTemplate.CAMP),
+    MARBLE("Marble", "LUXURY", 0, 0, 2, ImprovementTemplate.QUARRY),
+    SILK("Silk", "LUXURY", 0, 0, 2, ImprovementTemplate.PLANTATION),
+    SILVER("Silver", "LUXURY", 0, 0, 2, ImprovementTemplate.MINE),
+    SUGAR("Sugar", "LUXURY", 0, 0, 2, ImprovementTemplate.PLANTATION);
 
     private String name;
     private String type;
@@ -27,13 +27,15 @@ public enum ResourceTemplate {
     private int production;
     private int gold;
     private int eraNumber;
+    private ImprovementTemplate requiredImprovement;
     // TODO: Add required improvement, technology
 
-    ResourceTemplate(String name, String type, int food, int production, int gold) {
+    ResourceTemplate(String name, String type, int food, int production, int gold, ImprovementTemplate requiredImprovement) {
         this.name = name;
         this.type = type;
         this.food = food;
         this.production = production;
         this.gold = gold;
+        this.requiredImprovement = requiredImprovement;
     }
 }
