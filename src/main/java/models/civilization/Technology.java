b5142ce2b2fa19ab167aec2aad1a79b35aa95dc6
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-04-23 15:43:55 +0430

    Add import

--- src/main/java/models/civilization/Technology.java
+++ src/main/java/models/civilization/Technology.java
@@ -1,5 +1,7 @@
 package models.civilization;
 
+import models.civilization.enums.TechnologyTemplate;
+
 public class Technology {
     private TechnologyTemplate technologyTemplate;
     private int progress;
