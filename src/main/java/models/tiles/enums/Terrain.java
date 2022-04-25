ea733c95b4391bd71a577a7eedad46fa0c10ec41
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-04-25 05:44:03 +0430

    Add color to terrains

--- src/main/java/models/tiles/enums/Terrain.java
+++ src/main/java/models/tiles/enums/Terrain.java
@@ -1,18 +1,19 @@
 package models.tiles.enums;
 
 import models.tiles.TerrainOrTerrainFeature;
+import views.enums.Color;
 
 import java.util.Random;
 
 public enum Terrain implements TerrainOrTerrainFeature {
-    DESERT("Desert", 0, 0, 0, 1, -1/3, true),
-    GRASSLAND("Grassland", 2, 0, 0, 1, -1/3, true),
-    HILL("Hill", 0, 2, 0, 2, 1/4, true),
-    MOUNTAIN("Mountain", 0, 0, 0, 0, 0, false),
-    OCEAN("Ocean", 0, 0, 0, 0, 0, false),
-    PLAIN("Plain", 1, 1, 0, 1, -1/3, true),
-    SNOW("Snow", 0, 0, 0, 1, -1/3, true),
-    TUNDRA("Tundra", 1, 0, 0, 1, -1/3, true);
+    DESERT("Desert", 0, 0, 0, 1, -1/3, true, Color.YELLOW_BACKGROUND),
+    GRASSLAND("Grassland", 2, 0, 0, 1, -1/3, true, Color.GREEN_BACKGROUND),
+    HILL("Hill", 0, 2, 0, 2, 1/4, true, Color.MAGENTA_BACKGROUND),
+    MOUNTAIN("Mountain", 0, 0, 0, 0, 0, false, Color.BLACK_BACKGROUND),
+    OCEAN("Ocean", 0, 0, 0, 0, 0, false, Color.BLUE_BACKGROUND),
+    PLAIN("Plain", 1, 1, 0, 1, -1/3, true, Color.CYAN_BACKGROUND),
+    SNOW("Snow", 0, 0, 0, 1, -1/3, true, Color.WHITE_BACKGROUND),
+    TUNDRA("Tundra", 1, 0, 0, 1, -1/3, true, Color.GREEN_BACKGROUND);
     private String name;
     private int food;
     private int production;
@@ -20,8 +21,8 @@ public enum Terrain implements TerrainOrTerrainFeature {
     private int movementCost;
     private float combatModifiers;
     private boolean accessible;
-
-    Terrain(String name, int food, int production, int gold, int movementCost, float combatModifiers, boolean accessible) {
+    private Color color;
+    Terrain(String name, int food, int production, int gold, int movementCost, float combatModifiers, boolean accessible, Color color) {
         this.name = name;
         this.food = food;
         this.production = production;
@@ -29,6 +30,7 @@ public enum Terrain implements TerrainOrTerrainFeature {
         this.movementCost = movementCost;
         this.combatModifiers = combatModifiers;
         this.accessible = accessible;
+        this.color = color;
     }
 
     public static Terrain generateRandomTerrain(Random random) {
@@ -62,7 +64,13 @@ public enum Terrain implements TerrainOrTerrainFeature {
         return combatModifiers;
     }
 
+    public Color getColor() {
+        return color;
+    }
+
     public boolean isAccessible() {
         return accessible;
     }
+
+
 }
