03e2d88fd6f0e9b6f7137ab33f728fa11ed1b846
ahjafar <amir138106@gmail.com>
2022-04-25 06:31:50 +0430

    add map sign

--- src/main/java/models/tiles/enums/TerrainFeature.java
+++ src/main/java/models/tiles/enums/TerrainFeature.java
@@ -5,14 +5,15 @@ import models.tiles.TerrainOrTerrainFeature;
 import java.util.Random;
 
 public enum TerrainFeature implements TerrainOrTerrainFeature {
-    FOOD_PLAIN("Food plain", 2, 0, 0, 1, -1/3, true),
-    FOREST("Forest", 1, 1, 0, 2, 1/4, true),
-    ICE("Ice", 0, 0, 0, 0, 0, false),
-    JUNGLE("Jungle", 1, -1, 0, 2, 1/4, true),
-    MARSH("Marsh", -1, 0, 0, 2, -1/3, true),
-    OASIS("Oasis", 3, 0, 1, 1, -1/3, true);
+    FOOD_PLAIN("Food plain","FPlain ", 2, 0, 0, 1, -1/3, true),
+    FOREST("Forest","Forest ", 1, 1, 0, 2, 1/4, true),
+    ICE("Ice","  Ice  ", 0, 0, 0, 0, 0, false),
+    JUNGLE("Jungle","Jungle ", 1, -1, 0, 2, 1/4, true),
+    MARSH("Marsh"," Marsh ", -1, 0, 0, 2, -1/3, true),
+    OASIS("Oasis"," Oasis ", 3, 0, 1, 1, -1/3, true);
 
     private String name;
+    private String mapSign;
     private int food;
     private int production;
     private int gold;
@@ -20,8 +21,9 @@ public enum TerrainFeature implements TerrainOrTerrainFeature {
     private float combatModifier;
     private boolean accessible;
 
-    TerrainFeature(String name, int food, int production, int gold, int movementCost, float combatModifier, boolean accessible) {
+    TerrainFeature(String name,String mapSign, int food, int production, int gold, int movementCost, float combatModifier, boolean accessible) {
         this.name = name;
+        this.mapSign = mapSign;
         this.food = food;
         this.production = production;
         this.gold = gold;
@@ -29,6 +31,10 @@ public enum TerrainFeature implements TerrainOrTerrainFeature {
         this.combatModifier = combatModifier;
         this.accessible = accessible;
 
+    }
+
+    public String getMapSign() {
+        return mapSign;
     }
 
     public static TerrainFeature generateRandomTerrainFeature(Random random) {
