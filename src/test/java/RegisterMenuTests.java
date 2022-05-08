196593d9fc32b2df51f6f7a7c52d5e319490797c
saj306 <s.a.jafari306@gmail.com>
2022-05-08 18:32:08 +0430

    Add test folder

---
+++ src/test/java/RegisterMenuTests.java
@@ -0,0 +1,10 @@
+
+import controllers.RegisterMenuController;
+import models.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RegisterMenuTests {
+
+    
+}
