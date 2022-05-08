6faac52e76d102a06786af4d6f0c1f2b6478491c
saj306 <s.a.jafari306@gmail.com>
2022-05-08 22:48:23 +0430

    Add some tests

--- src/test/java/RegisterMenuTests.java
+++ src/test/java/RegisterMenuTests.java
@@ -3,14 +3,15 @@ import controllers.RegisterMenuController;
 import models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import views.enums.Message;
 
 public class RegisterMenuTests {
 
 
-    @BeforeAll
-    public static void setUp(){
+    @BeforeEach
+    public void setUp(){
         User.getAllUsers().clear();
     }
 
@@ -25,6 +26,19 @@ public class RegisterMenuTests {
         RegisterMenuController.addUser("ali" , "110" , "ali110");
         Message message = RegisterMenuController.checkUserRegisterData("ali","110","mahdi110");
         Assertions.assertEquals(Message.USERNAME_EXISTS , message);
+    }
+    @Test
+    public void checkUserRegisterDataWithSameNickname(){
+        RegisterMenuController.addUser("ali" , "110" , "ali110");
+        Message message = RegisterMenuController.checkUserRegisterData("mahdi","110","ali110");
+        Assertions.assertEquals(Message.NICKNAME_EXISTS , message);
+    }
+
+    @Test
+    public void checkUserRegisterDataSuccessful(){
+        RegisterMenuController.addUser("ali" , "110" , "ali110");
+        Message message = RegisterMenuController.checkUserRegisterData("mahdi","110","mahdi110");
+        Assertions.assertEquals(Message.SUCCESS , message);
     }
 
 
