7afd868ca10f117e40bb8c28bb6c2993269c109c
saj306 <s.a.jafari306@gmail.com>
2022-06-05 01:26:21 +0430

    Some edit :)

--- src/main/java/views/RegisterPage.java
+++ src/main/java/views/RegisterPage.java
@@ -2,9 +2,16 @@ package views;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class RegisterPage extends PageController {
 
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField nickname;
+    @FXML
+    private TextField password;
     @FXML
     private Label error;
 }
