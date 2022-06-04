c19e365c1254fb532e909ff32e4e8ee569f9faff
saj306 <s.a.jafari306@gmail.com>
2022-06-05 02:02:38 +0430

    Edit register page

--- src/main/java/views/RegisterPage.java
+++ src/main/java/views/RegisterPage.java
@@ -1,8 +1,13 @@
 package views;
 
+import controllers.RegisterMenuController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import views.enums.Message;
+
+import java.io.IOException;
 
 public class RegisterPage extends PageController {
 
@@ -14,4 +19,40 @@ public class RegisterPage extends PageController {
     private TextField password;
     @FXML
     private Label error;
+
+
+    private void checkDataOfUser(){
+        Message message = RegisterMenuController.checkUserRegisterData(username.getText() , password.getText() , nickname.getText());
+        String textOfError = "";
+        if(username.getText().length() != 0 && password.getText().length() != 0 && nickname.getText().length() != 0)
+        {
+            switch (message) {
+                case SUCCESS:
+                    textOfError = "user created successfully!";
+                    error.setStyle("-fx-text-fill: green");
+                    break;
+                case USERNAME_EXISTS:
+                    textOfError = "user with username " + username.getText() + " already exists";
+                    error.setStyle("-fx-text-fill: #ea540a");
+                    break;
+                case NICKNAME_EXISTS:
+                    textOfError = "user with nickname " + nickname.getText() + " already exists";
+                    error.setStyle("-fx-text-fill: #ea540a");
+                    break;
+                default:
+                    break;
+            }
+        }
+        else {
+            textOfError = "some field are empty!";
+            error.setStyle("-fx-text-fill: #ea540a");
+        }
+
+        error.setText(textOfError);
+        error.setLayoutX(340- textOfError.length()*4.5);
+    }
+
+    public void goLoginMenu(MouseEvent mouseEvent) throws IOException {
+        checkDataOfUser();
+    }
 }
