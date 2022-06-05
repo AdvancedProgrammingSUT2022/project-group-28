46a46d64574668859350f96be795bb269c2fbdcf
ahjafar <amir138106@gmail.com>
2022-06-05 16:41:18 +0430

    basic profile menu

---
+++ src/main/java/views/Profile.java
@@ -0,0 +1,33 @@
+package views;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class Profile extends PageController{
+    @FXML
+    private ImageView picture;
+
+    public void back(){
+
+    }
+
+    public void chooseAvatar(){
+
+    }
+
+    public void leftPic(){
+
+    }
+
+    public void rightPic(){
+
+    }
+
+    public void changePassword(){
+
+    }
+
+    public void changeNickname(){
+
+    }
+}
