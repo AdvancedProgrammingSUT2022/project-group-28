7e34ecadf9bebbcde7a7111f5bd28f107e0802af
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-06-06 18:08:55 +0430

    Add create map

--- src/main/java/views/GamePage.java
+++ src/main/java/views/GamePage.java
@@ -1,7 +1,9 @@
 package views;
 
 import controllers.GameController;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import views.components.Hex;
 
@@ -12,11 +14,24 @@ public class GamePage extends PageController{
 
     @FXML
     private void initialize() {
-        Double[] center = new Double[]{
-                300.0, 300.0,
-        };
-        Hex hex = new Hex(GameController.getGame().getMap()[30][30], center);
 
-        pane.getChildren().add(hex);
+        createMap(30, 30);
+    }
+
+    private void createMap(int baseI, int baseJ) {
+        this.pane.getChildren().clear();
+        for (int j = 0; j < 14; j++) {
+            for (int i = 0; i < 7; i++) {
+                System.out.println(i + "," + j);
+                int tileI = baseI + i - 3;
+                int tileJ = baseJ + j - i/2 - 7;
+                double hexX = tileJ * 173.2 + tileI * 86.6 - baseJ * 230;
+                double hexY = tileI * 150 - baseI * 135;
+                System.out.println(tileI + "," + tileJ);
+                System.out.println(hexX + "," + hexY);
+                Hex hex = new Hex(GameController.getGame().getMap()[tileI][tileJ], hexX, hexY);
+                this.pane.getChildren().add(hex);
+            }
+        }
     }
 }
--- src/main/java/views/components/Hex.java
+++ src/main/java/views/components/Hex.java
@@ -1,8 +1,11 @@
 package views.components;
 
+import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
 import models.tiles.TerrainOrTerrainFeature;
 import models.tiles.Tile;
 import models.tiles.enums.Terrain;
@@ -11,8 +14,9 @@ import views.App;
 
 import java.util.HashMap;
 
-public class Hex extends Polygon {
+public class Hex extends Group {
     private static HashMap<TerrainOrTerrainFeature, ImagePattern> terrainImages = new HashMap<>();
+
     static {
         for (Terrain terrain : Terrain.values()) {
             Image image = new Image(App.class.getResource("../assets/image/terrain/" + terrain.getFilename() + ".png").toExternalForm());
@@ -24,34 +28,57 @@ public class Hex extends Polygon {
         }
     }
 
-
     private final Double RADIUS = 100d;
 
     private Tile tile;
-    private Double[] center;
-    public Hex(Tile tile, Double[] center) {
-        this.tile = tile;
-        this.center = center;
-        setInitialPoints();
-        setHexPicture();
 
+    // Components
+    private Polygon mainPolygon;
+    private Text banner;
+
+    public Hex(Tile tile, Double x, Double y) {
+        this.tile = tile;
+        this.setLayoutX(x);
+        this.setLayoutY(y);
+
+        this.mainPolygon = new Polygon();
+        setPolygonPoints(this.mainPolygon);
+        updateTerrainPicture();
+
+        this.getChildren().add(this.mainPolygon);
+
+        this.banner = new Text(tile.getCoordinates()[0] + "," + tile.getCoordinates()[1]);
+        this.getChildren().add(this.banner);
     }
 
-    private void setInitialPoints() {
+    private void setPolygonPoints(Polygon polygon) {
         Double[] points = new Double[12];
 
-
         for (int i = 0; i < 6; i++) {
-            points[i * 2] = this.center[0] - Math.sin(Math.PI/3 * i) * this.RADIUS;
-            points[i * 2 + 1] = this.center[1] + Math.cos(Math.PI/3 * i) * this.RADIUS;
+            points[i * 2] = -Math.sin(Math.PI/3 * i) * this.RADIUS;
+            points[i * 2 + 1] = Math.cos(Math.PI/3 * i) * this.RADIUS;
         }
-        this.getPoints().addAll(points);
+        polygon.getPoints().addAll(points);
     }
 
-    private void setHexPicture() {
+    private void updateTerrainPicture() {
         if (this.tile.getTerrainFeature() != null) {
-            this.setFill(terrainImages.get(this.tile.getTerrainFeature()));
-        } else this.setFill(terrainImages.get(this.tile.getTerrain()));
+            this.mainPolygon.setFill(terrainImages.get(this.tile.getTerrainFeature()));
+        } else this.mainPolygon.setFill(terrainImages.get(this.tile.getTerrain()));
+    }
+
+    public void move(Double x, Double y) {
+//        this.center[0] = this.center[0] + x;
+//        this.center[1] = this.center[1] + y;
+//        for (int i = 0; i < 12; i++) {
+//            Double temp = this.getPoints().get(i);
+//            this.getPoints().remove(i);
+//            if (i%2 == 0) this.getPoints().add(i, temp + x);
+//            else this.getPoints().add(i, temp + y);
+//        }
+        this.setLayoutX(this.getLayoutX() + x);
+        this.setLayoutY(this.getLayoutY() + y);
+
     }
 
 }
