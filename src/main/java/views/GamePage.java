72e000c8e1d6133499d3c9c3672de70defdbb961
ahjafar <amir138106@gmail.com>
2022-06-06 18:24:18 +0430

    keyboard map movement

--- src/main/java/views/GamePage.java
+++ src/main/java/views/GamePage.java
@@ -1,9 +1,8 @@
 package views;
 
 import controllers.GameController;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import views.components.Hex;
 
@@ -11,27 +10,46 @@ public class GamePage extends PageController{
     @FXML
     private Pane pane;
 
+    private int offsetI=0,offsetJ=0;
+
 
     @FXML
     private void initialize() {
-
         createMap(30, 30);
+        
     }
 
     private void createMap(int baseI, int baseJ) {
         this.pane.getChildren().clear();
-        for (int j = 0; j < 14; j++) {
-            for (int i = 0; i < 7; i++) {
-                System.out.println(i + "," + j);
-                int tileI = baseI + i - 3;
-                int tileJ = baseJ + j - i/2 - 7;
-                double hexX = tileJ * 173.2 + tileI * 86.6 - baseJ * 230;
-                double hexY = tileI * 150 - baseI * 135;
-                System.out.println(tileI + "," + tileJ);
-                System.out.println(hexX + "," + hexY);
+        for (int j = 0; j < 100; j++) {
+            for (int i = 0; i < 100; i++) {
+                //System.out.println(i + "," + j);
+                int tileI = baseI + i - 50;
+                int tileJ = baseJ + j - i/2 - 50;
+                double hexX = tileJ * 173.2 + tileI * 86.6 - baseJ * 230 + offsetI;
+                double hexY = tileI * 150 - baseI * 135 + offsetJ;
+                //System.out.println(tileI + "," + tileJ);
+                //System.out.println(hexX + "," + hexY);
+                if(hexX<-100 || hexX>=1700 || hexY<-100 || hexY>=1000)
+                    continue;
                 Hex hex = new Hex(GameController.getGame().getMap()[tileI][tileJ], hexX, hexY);
                 this.pane.getChildren().add(hex);
             }
         }
+    }
+
+    @FXML
+    private void keyPressed(KeyEvent keyEvent){
+        // TODO:cant move out of page
+        if(keyEvent.getCode().getName().equals("Up")){
+            offsetJ-=5;
+        } else if(keyEvent.getCode().getName().equals("Down")){
+            offsetJ+=5;
+        } else if(keyEvent.getCode().getName().equals("Left")){
+            offsetI-=5;
+        } else if(keyEvent.getCode().getName().equals("Right")){
+            offsetI+=5;
+        }
+        createMap(30, 30);
     }
 }
