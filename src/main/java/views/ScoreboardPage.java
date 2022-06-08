e77ff1602a89b55aa3f331f0b72d1fc6c01495d1
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-06-08 16:34:42 +0430

    Add real users

--- src/main/java/views/ScoreboardPage.java
+++ src/main/java/views/ScoreboardPage.java
@@ -17,11 +17,10 @@ public class ScoreboardPage extends PageController {
 
     @FXML
     public void initialize() {
-        User user = new User("a", "mohsen", "a");
-        for (int i = 0; i < 10; i++) {
+        // TODO: add sort users
+        for (User user : User.getAllUsers()) {
             rankings.getChildren().add(createUserInfoHBox(user));
         }
-
     }
 
     private HBox createUserInfoHBox(User user) {
@@ -30,10 +29,8 @@ public class ScoreboardPage extends PageController {
         hBox.setPrefWidth(700);
         hBox.getStyleClass().add("user_ranking_info");
 
-        // TODO: change profile image
-        ImagePattern imagePattern = new ImagePattern(new Image(App.class.getResource("../assets/image/background/start_background.png").toExternalForm()));
         Circle avatar = new Circle(40);
-        avatar.setFill(imagePattern);
+        avatar.setFill(new ImagePattern(user.getAvatar()));
         hBox.getChildren().add(avatar);
 
         VBox nicknameVBox = new VBox();
@@ -44,14 +41,12 @@ public class ScoreboardPage extends PageController {
 
         VBox scoreVBox = new VBox();
         scoreVBox.setPrefWidth(200);
-        Text score = new Text(String.valueOf(user.getScore() + 100000));
+        Text score = new Text(String.valueOf(user.getScore()));
         scoreVBox.getChildren().add(score);
         hBox.getChildren().add(scoreVBox);
 
         return hBox;
     }
-
-    // TODO: add start game stuff
 
     @FXML
     private void back() {
