c765c8f14fca21f7d72d6ebe8b88b397cf01d776
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-06-08 11:21:56 +0430

    Add map boundaries

--- src/main/java/views/GamePage.java
+++ src/main/java/views/GamePage.java
@@ -7,18 +7,18 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
+import models.tiles.Tile;
+import models.tiles.enums.Terrain;
 import views.components.Hex;
 
 public class GamePage extends PageController{
     @FXML
     private AnchorPane pane;
-    @FXML
-    private Rectangle right, left, top, bottom;
 
     private boolean rightKey = false, leftKey = false, topKey = false, bottomKey = false;
     private boolean rightMouse = false, leftMouse = false, topMouse = false, bottomMouse = false;
 
-    private int offsetI=0,offsetJ=0;
+    private int offsetI=0, offsetJ=0;
 
 
     @FXML
@@ -35,15 +35,20 @@ public class GamePage extends PageController{
 
     private void createMap(int baseI, int baseJ) {
         this.pane.getChildren().clear();
-        for (int j = 0; j < 100; j++) {
-            for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 200; j++) {
+            for (int i = 0; i < 200; i++) {
                 int tileI = baseI + i - 50;
                 int tileJ = baseJ + j - i/2 - 50;
                 double hexX = tileJ * 173.2 + tileI * 86.6 - baseJ * 230 + offsetI;
                 double hexY = tileI * 150 - baseI * 135 + offsetJ;
-                if(hexX<-100 || hexX>=1700 || hexY<-100 || hexY>=1000)
+                if (hexX<-100 || hexX>=1700 || hexY<-100 || hexY>=1000)
                     continue;
-                Hex hex = new Hex(GameController.getGame().getMap()[tileI][tileJ], hexX, hexY);
+
+                Tile tile;
+                if (tileI < 0 || tileI >= 100 || tileJ < 0 || tileJ >= 100) {
+                    tile = new Tile(tileI, tileJ, Terrain.OCEAN, null, null, null);
+                } else tile = GameController.getGame().getMap()[tileI][tileJ];
+                Hex hex = new Hex(tile, hexX, hexY);
                 hex.setMouseTransparent(true);
                 this.pane.getChildren().add(hex);
             }
@@ -78,11 +83,6 @@ public class GamePage extends PageController{
     }
 
     @FXML
-    private void rightEntered(MouseEvent mouseEvent){
-        rightMouse = true;
-    }   
-
-    @FXML
     private void mouseMoved(MouseEvent mouseEvent){
         leftMouse = (mouseEvent.getX()>20)?false:true;
         topMouse = (mouseEvent.getY()>20)?false:true;
@@ -90,18 +90,26 @@ public class GamePage extends PageController{
         rightMouse = (mouseEvent.getX()<1580)?false:true;
     }
 
-    private void update(){
-        if(rightKey || rightMouse){
-            offsetI-=5;
+    private void update() {
+        if(rightKey || rightMouse) {
+            if (offsetI * 1.72 + 20295 > offsetJ) {
+                offsetI -= 5;
+            }
         }
         if(leftKey || leftMouse){
-            offsetI+=5;
+            if (offsetI * 1.72 - 7615 < offsetJ) {
+                offsetI+=5;
+            }
         }
-        if(topKey || topMouse){
-            offsetJ+=5;
+        if(topKey || topMouse) {
+            if (offsetJ < 4150) {
+                offsetJ += 5;
+            }
         }
         if(bottomKey || bottomMouse){
-            offsetJ-=5;
+            if (offsetJ > -10250) {
+                offsetJ-=5;
+            }
         }
         createMap(30,30);
     }
