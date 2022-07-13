76e860f4f5f0d9dba82fef3d6e22153e1fc7d58b
MohsenGhasemi <mohsenghasemi8156@gmail.com>
2022-07-13 17:19:21 +0430

    next turn added

--- src/main/java/views/GameMediator.java
+++ src/main/java/views/GameMediator.java
@@ -3,6 +3,7 @@ package views;
 import controllers.CityController;
 import controllers.CivilizationController;
 import controllers.GameController;
+import controllers.GameMenuController;
 import controllers.units.UnitController;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -12,10 +13,14 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import models.Constructable;
 import models.civilization.City;
+import models.civilization.Civilization;
 import models.tiles.Tile;
 import models.units.Unit;
 import models.units.enums.UnitTemplate;
 import views.enums.CityMessage;
+import views.notifications.CivilizationNotification;
+import views.notifications.GameNotification;
+import views.notifications.NotificationTemplate;
 
 public class GameMediator {
     private static GameMediator instance;
@@ -125,5 +130,39 @@ public class GameMediator {
         int i = destination.getCoordinates()[0];
         int j = destination.getCoordinates()[1];
         UnitController.moveUnitToTarget(i, j);
+    }
+
+    public void nextTurn() {
+        GameNotification gameNotification = GameMenuController.nextTurn();
+        NotificationTemplate notification = gameNotification.getNotificationTemplate();
+        // TODO: change add message template
+        if (notification == CivilizationNotification.FREE_UNIT) {
+            HUDController.getInstance().addMessage("You have free unit");
+            int freeUnitI = Integer.parseInt(gameNotification.getData().get(0));
+            int freeUnitJ = Integer.parseInt(gameNotification.getData().get(1));
+            GamePage.getInstance().setBaseI(freeUnitI);
+            GamePage.getInstance().setBaseJ(freeUnitJ);
+            GamePage.getInstance().setOffsetI(0);
+            GamePage.getInstance().setOffsetJ(0);
+        } else if (notification == CivilizationNotification.NO_CONSTRUCTION) {
+            HUDController.getInstance().addMessage("The city has no construction");
+            CityController.selectCityByName(gameNotification.getData().get(0), false);
+            City city = GameController.getGame().getSelectedCity();
+            int cityI = city.getTile().getCoordinates()[0];
+            int cityJ = city.getTile().getCoordinates()[1];
+            GamePage.getInstance().setBaseI(cityI);
+            GamePage.getInstance().setBaseJ(cityJ);
+            GamePage.getInstance().setOffsetI(0);
+            GamePage.getInstance().setOffsetJ(0);
+        } else if (notification == CivilizationNotification.NO_TECHNOLOGY) {
+            System.out.println("tech required");
+        } else {
+            System.out.println("done");
+            // TODO: notify server
+            // TODO: handle start new turn
+            GameMenuController.startNewTurn();
+
+        }
+        GamePage.getInstance().createMap(true);
     }
 }
