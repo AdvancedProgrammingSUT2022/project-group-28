07c04883b0dbf442b69a0da9b8310e445ef52320
ahjafar <amir138106@gmail.com>
2022-07-19 20:32:41 +0430

    remove old main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,22 +1,7 @@
 import views.App;
-import views.Menu;
-
-class OldMain {
-    public static void main(String[] args) {
-        while(true){
-            Menu.getCurrentMenu().run();
-        }
-    }
-}
 
 public class Main{
     public static void main(String[] args) {
-        new Thread(new Runnable(){
-            @Override
-            public void run() {
-                OldMain.main(args);
-            }
-        }).start();
         App.run(Main.class.getResource(""));
     }
 
