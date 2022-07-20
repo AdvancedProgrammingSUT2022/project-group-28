5831e4e6ee39193a72205b1f118633a3e8ce8bfa
saj306 <s.a.jafari306@gmail.com>
2022-07-20 18:07:06 +0430

    Add borderpane for technology menu

---
+++ client/src/main/java/views/components/TechnologyInfo.java
@@ -0,0 +1,19 @@
+package views.components;
+
+import javafx.scene.Group;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+public class ThechnologyInfo extends Group {
+    private VBox thechnologyInfo;
+    private Text text;
+    private Circle icon;
+
+    public ThechnologyInfo(){
+        thechnologyInfo = new VBox(20);
+        
+    }
+
+}
