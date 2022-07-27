32b2c312544395ccc7d41489815acddcc1ffaece
saj306 <s.a.jafari306@gmail.com>
2022-07-27 13:06:44 +0430

    Change some things

--- server/src/main/java/controllers/Database.java
+++ server/src/main/java/controllers/Database.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.AnyTypePermission;
 import models.Chat;
 import models.FriendshipRequest;
 import models.User;
@@ -10,22 +11,16 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Database {
-    public static Connection connection = null;
-    public static ResultSet rs = null;
-    public static Statement statement = null;
-    public Database() throws ClassNotFoundException {
+    public Connection connect() throws ClassNotFoundException {
         // load the sqlite-JDBC driver using the current class loader
         Class.forName("org.sqlite.JDBC");
+        Connection connection = null;
+
         try {
             // create a database connection
             connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-            statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-            statement.executeUpdate("drop table if exists person");
-            statement.executeUpdate("create table person (id integer, username string , nickname string , password string ," +
-                    " score integer , profilePicNumber integer , lastOnline string , lastWin string ,online integer ," +
-                    "friends string , friendshipRequests string , chats string )");
+//
 
 
         } catch (SQLException e) {
@@ -33,14 +28,19 @@ public class Database {
             // it probably means no database file is found
             System.err.println(e.getMessage());
         }
+        return connection;
     }
 
     public void insertData(int id, String username, String nickname, String password , int score , int profilePicNumber ,
                 String lastOnline , String lastWin , int online , String friends , String friendshipRequests ,String chats ) {
+
         String sql = "INSERT INTO person(id , username , nickname , password , score , profilePicNumber , " +
                 " lastOnline ,lastWin ,  online ,friends ,friendshipRequests ,chats) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+        Connection connection =null;
+        PreparedStatement pstmt = null;
         try{
-            PreparedStatement pstmt = connection.prepareStatement(sql);
+            connection = this.connect();
+            pstmt = connection.prepareStatement(sql);
             pstmt.setInt(1, id);
             pstmt.setString(2, username);
             pstmt.setString(3,nickname);
@@ -53,17 +53,25 @@ public class Database {
             pstmt.setString(10, friends);
             pstmt.setString(11,friendshipRequests);
             pstmt.setString(12,chats);
-
             pstmt.executeUpdate();
-        } catch (SQLException e) {
+
+        } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
     }
     public void exportData(){
         XStream xStream = new XStream();
+        xStream.addPermission(AnyTypePermission.ANY);
+        Connection connection =null;
+        ResultSet rs = null;
+        PreparedStatement statement = null;
         try {
-            rs = statement.executeQuery("select * from person");
+            connection = this.connect();
+            String sql = "SELECT * FROM person";
+            statement = connection.prepareStatement(sql);
+            rs = statement.executeQuery();
+            ArrayList<User> users = new ArrayList<>();
             while (rs.next()) {
                 User user = new User(rs.getString("username") , rs.getString("nickname") , rs.getString("password"));
                 user.setId(rs.getInt("id"));
@@ -75,12 +83,38 @@ public class Database {
                 user.setFriends((ArrayList<User>) xStream.fromXML(rs.getString("friends")));
                 user.setFriendshipRequests((ArrayList<FriendshipRequest>) xStream.fromXML(rs.getString("friendshipRequests")));
                 user.setChats((ArrayList<Chat>) xStream.fromXML(rs.getString("chats")));
+                users.add(user);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            User.getAllUsers().clear();
+            User.setAllUsers(users);
+        } catch (SQLException | ClassNotFoundException e) {
+            try {
+                Statement statement1 = null;
+                statement1 = connection.createStatement();
+                statement1.setQueryTimeout(30);  // set timeout to 30 sec.
+                statement1.executeUpdate("drop table if exists person");
+                statement1.executeUpdate("create table IF NOT EXISTS person (id integer, username string , nickname string , password string ," +
+                        " score integer , profilePicNumber integer , lastOnline string , lastWin string ,online integer ," +
+                        "friends string , friendshipRequests string , chats string )");
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
 
     }
 
+    public void deleteAllData() {
+        String deleteSql = "delete from person";
+        Connection connection =null;
+        PreparedStatement pstmt = null;
+        try {
+            connection = this.connect();
+            pstmt = connection.prepareStatement(deleteSql);
+            pstmt.executeUpdate();
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
 
